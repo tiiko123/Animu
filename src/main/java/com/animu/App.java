09257338f6048d4a6fba1239ea492d7a9a019699
Francisco <1830275@upv.edu.mx>
2020-04-19 17:42:42 -0500

    Diseño del login

--- src/main/java/com/animu/App.java
+++ src/main/java/com/animu/App.java
@@ -1,14 +1,25 @@
 package com.animu;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends Application {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("Nuevo commit");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage =(Stage) FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.show();
     }
 }
